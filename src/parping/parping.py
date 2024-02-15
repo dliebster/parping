27f51ea0db0d546400fa294e14dd2dfd28ee2eda
dliebster <daniel.liebster@gmail.com>
2024-02-15 10:02:03 -0500

    refactoring parping_all, take one

--- src/parping/parping.py
+++ src/parping/parping.py
@@ -19,7 +19,6 @@ import re
 
 ostype = platform.system()
 FNULL = open(os.devnull, 'w')
-goodip = badip = 0
 indata = "none"
 ips = []
 iplist = []
@@ -74,65 +73,11 @@ while indata == "none":
             ips.append(item)
 
 
-def parping_all():
-    ostype = platform.system()
-    FNULL = open(os.devnull, 'w')
+def parping_all(ips_in):
     goodip = badip = 0
-    indata = "none"
-    ips = []
-    iplist = []
-    # The following is regex to check if an ip address was supplied as an arg.
-    ippatt = re.compile(
-        ('^([2][0-5][0-5]|^[1]{0,1}[0-9]{1,2})'
-         '\.([0-2][0-5][0-5]|[1]{0,1}[0-9]{1,2})'
-         '\.([0-2][0-5][0-5]|[1]{0,1}[0-9]{1,2})'
-         '\.([0-2][0-5][0-5]|[1]{0,1}[0-9]{1,2})$'))
-
-    while indata == "none":
-        if len(sys.argv) > 1:
-
-            indata = "cli"
-            userargs = sys.argv[1]
-            ipmatch = re.search(ippatt, userargs.split("-")[0])
-
-            if ipmatch:
-
-                iprange = userargs.split(".")
-                a = iprange[0]
-                b = iprange[1]
-                c = iprange[2]
-                d = iprange[3]
-
-                drange = d.split("-")
-                dbegin = int(drange[0])
-                dend = int(drange[1])
-
-                index = dbegin
-
-                while index <= dend:
-                    itemip = a + "." + b + "." + c + "." + str(index)
-                    ips.append(itemip)
-                    index += 1
-
-
-        else:
-            indata = "filelist"
-            try:
-                ipfile = open('iplist', 'r')
-            except IOError as e:
-                print(
-                    "\n\n Expecting to find file named \"iplist\" given the lack of subnet iprange argument. ({0}): {1}\n\n".format(
-                        e.errno, e.strerror))
-                exit()
-            except:
-                print("Unexpected error:", sys.exc_info()[0])
-                raise
-
-            for item in ipfile:
-                ips.append(item)
 
     p = {}
-    for ip in ips:
+    for ip in ips_in:
         if not ip.isspace():
             if ostype == "Darwin":
                 p[ip] = subprocess.Popen(['ping', '-n', '-W2', '-c2', ip], stdout=FNULL, stderr=subprocess.STDOUT)
